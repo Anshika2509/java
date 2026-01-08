e6b8359ca845c3648f605c0af4c73d1a6821e047
Anshika2509 <anshika25aneja09@gmail.com>
2026-01-08 20:43:34 +0530

    jagged array intro

---
+++ JaggedArray.java
@@ -0,0 +1,24 @@
+public class JaggedArray {
+    public static void main(String[] args) {
+        //Jagged array: array with different number of columns  
+        // refer to https://github.com/Anshika2509/java/blob/main/jagged-array.png
+
+        int num[][] = new int[3][];
+        num[0] = new int[3];
+        num[1] = new int[2];
+        num[2] = new int[4];
+
+        for(int i = 0; i< num.length; i++){
+            for (int j = 0; j<num[i].length; j++) {
+                num[i][j] = (int)(Math.random()*10);
+            }
+        }
+
+        for(int i = 0; i< num.length; i++){
+            for (int j = 0; j<num[i].length; j++) {
+                System.out.print(num[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+}
